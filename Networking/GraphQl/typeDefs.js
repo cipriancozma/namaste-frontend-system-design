eed13c3456dc020b264690d1abaa029270d6fa08
Ciprian <cozma_ciprian19@yahoo.com>
2024-04-22 15:25:04 +0300

    refactored files

---
+++ Networking/GraphQl/typeDefs.js
@@ -0,0 +1,20 @@
+const typeDefs = `
+    type Author {
+        id: ID!
+        name: String!
+    }
+
+    type Book {
+        id: ID!
+        title: String!
+        publishedYear: Int
+    }
+
+    type Query {
+        
+    }
+
+    type Mutation {
+
+    }
+`;
